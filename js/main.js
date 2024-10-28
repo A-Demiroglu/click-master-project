be44d0347790be43d5a2525e04affaa809a7114e
A-Demiroglu <azad_demiroglu@hotmail.com>
2024-10-28 16:29:59 +0100

    Beetje javascript gemaakt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+let score = 0;
 
+document.getElementById('clickButton').addEventListener('click', function () {
+    score++;
+    document.getElementById('score').innerText = 'Totale Kliks: ' + score;
+});
+document.getElementById('resetButton').addEventListener('click', function () {
+    resetGame();
+});
+
+function resetGame() {
+    score = 0;
+    document.getElementById('score').innerText = 'Totale Kliks: ' + score;
+}
+
+let timeLeft = 15;
+let timerInterval;
+
+function startGame() {
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        document.getElementById('timer').innerText = 'Tijd: ' + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            alert('Tijd is om! Je score is: ' + score);
+        }
+    }, 1000);
+}
